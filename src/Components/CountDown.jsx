38959f73139cf13c4d05bec4be84436dc23fc919
SeikhAbubakar5 <128287400+SeikhAbubakar5@users.noreply.github.com>
2024-05-08 10:41:09 +0530

    Update CountDown.jsx

--- src/Components/CountDown.jsx
+++ src/Components/CountDown.jsx
@@ -19,7 +19,6 @@ const CountDown = () => {
         // Countdown finished
         setCountdownFinished(true); 
       } else {
-        //conditions
         const Days = Math.floor(totalTimeLeft / (1000 * 60 * 60 *24));
         const Hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
         const Minutes = Math.floor((totalTimeLeft / (1000 * 60)) % 60);
